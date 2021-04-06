7b073d3ae5364b5fe65f7a814d83eaf0411e8aaf
Josh Ourisman <me@josho.io>
2021-04-06 15:45:28 -0400

    Added ApolloProvider to app.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import 'tailwindcss/tailwind.css'
 import { Provider } from 'next-auth/client'
+import { ApolloProvider } from '@apollo/client'
+import client from '../apollo-client'
 
 function MyApp({ Component, pageProps }) {
   return <Provider session={pageProps.session}>
-    <Component {...pageProps} />
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
   </Provider>
 }
 
