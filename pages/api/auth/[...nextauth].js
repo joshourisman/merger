3799b5384da54bd88a3026874da2e1735d81e7ce
Josh Ourisman <me@josho.io>
2021-04-06 15:19:56 -0400

    Request more scopes from GitHub OAuth.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,18 @@ export default NextAuth({
     providers: [
         Providers.GitHub({
             clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            scope: [
+                'user',
+                'public_repo',
+                'repo',
+                'repo_deployment',
+                'repo:status',
+                'read:repo_hook',
+                'read:org',
+                'read:public_key',
+                'read:gpg_key',
+            ]
         }),
     ]
 })
