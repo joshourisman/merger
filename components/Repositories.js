505bdf9b755dc063133283237ceb972fca9f0f7c
Josh Ourisman <me@josho.io>
2021-04-07 14:18:41 -0400

    More efficient querying.

--- components/Repositories.js
+++ components/Repositories.js
@@ -2,43 +2,40 @@ import { useQuery, gql } from '@apollo/client'
 
 const QUERY = gql`
 query { 
-    viewer {
-      repositories(first: 100, ownerAffiliations: [OWNER]) {
-          nodes {
-            id
-            nameWithOwner
-            pullRequests(first: 100) {
-              nodes {
-                id
-                title
-              }
-            }
-          }
+  viewer {
+    repositories(first: 100, ownerAffiliations: [OWNER]) {
+      nodes {
+        id
+        nameWithOwner
+        pullRequests(first: 100, states: OPEN) {
+          totalCount
         }
       }
+    }
   }
+}
 `;
 
 export default function Repositories() {
-    const { data, loading, error } = useQuery(QUERY);
+  const { data, loading, error } = useQuery(QUERY);
 
-    if (loading) {
-        return <h2>Loading...</h2>;
-    }
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
 
-    if (error) {
-        console.log(error)
-        return <p>error...</p>
-    }
+  if (error) {
+    console.log(error)
+    return <p>error...</p>
+  }
 
-    const { viewer: { repositories: { nodes: repos } } } = data;
+  const { viewer: { repositories: { nodes: repos } } } = data;
 
-    return <div>
-        <h1>Repositories:</h1>
-        <ul>
-            {repos.map(({ nameWithOwner: repo, pullRequests: { nodes: pullRequests } }, index) => {
-                return <li key={index}>{repo} ({pullRequests.length})</li>
-            })}
-        </ul>
-    </div>
+  return <div>
+    <h1>Repositories:</h1>
+    <ul>
+      {repos.map(({ nameWithOwner: repo, pullRequests: { totalCount } }, index) => {
+        return <li key={index}>{repo} ({totalCount})</li>
+      })}
+    </ul>
+  </div>
 }
