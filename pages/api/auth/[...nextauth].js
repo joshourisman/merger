0e5d25b51800eac4de707cced05a79bcdd35b247
Josh Ourisman <me@josho.io>
2021-04-09 11:08:03 -0400

    Properly declare scopes.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,17 +6,7 @@ export default NextAuth({
         Providers.GitHub({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            scope: [
-                'user',
-                'public_repo',
-                'repo',
-                'repo_deployment',
-                'repo:status',
-                'read:repo_hook',
-                'read:org',
-                'read:public_key',
-                'read:gpg_key',
-            ]
+            scope: ['user,public_repo,repo,repo_deployment,repo:status,read:repo_hook,read:org,read:public_key,read:gpg_key']
         }),
     ],
     callbacks: {
